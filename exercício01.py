fa0d62ed08a716796a76436914ceeab73805aaf1
RobertMN076 <robert.giulio@escolar.ifrn.edu.br>
2024-07-10 20:19:06 -0300

    questao04

--- exercício01.py
+++ exercício01.py
@@ -1,9 +1,22 @@
 #Questão 01
-for i in range(1, 100+1, 1):
-    print(i, end = (","))
+# for i in range(1, 100+1, 1):
+#     print(i, end = (","))
 
 #Questão 02
-for i in range(100, 1-1, - 1):
-    print(i, end=(" ,"))
+# for i in range(100, 1-1, - 1):
+#     print(i, end=(" ,"))
+
+#Questão 04
+
+n = 10
+lista_de_numeros = []
+
+for i in range(n):
+    numeros = int(input(f"Digite o número {i+1}: "))
+    lista_de_numeros.append(numeros)
+
+soma = sum(lista_de_numeros)
+print(soma)
+
 
 
