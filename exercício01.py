94e18a72930f56de47b39b4398712d79f46b0cbf
JG1511 <joaotsi2024@gmail>
2024-07-10 21:03:47 -0300

    Questão07

--- exercício01.py
+++ exercício01.py
@@ -1,45 +1,57 @@
 #Questão 01
-for i in range(1, 100+1, 1):
-    print(i, end = (","))
+# for i in range(1, 100+1, 1):
+#     print(i, end = (","))
 
 #Questão 02
-for i in range(100, 1-1, - 1):
-    print(i, end=(" ,"))
+# for i in range(100, 1-1, - 1):
+#     print(i, end=(" ,"))
 
 #Questão 03
 
-var1 = int(input("Digite o primeiro valor: "))
-var2 = int(input("Digite o segundo valor: "))
+# var1 = int(input("Digite o primeiro valor: "))
+# var2 = int(input("Digite o segundo valor: "))
 
-varSoma = 0
+# varSoma = 0
 
-for i in range (var1, var2 +1):
+# for i in range (var1, var2 +1):
     
-    print(i, end=(","))
-    varSoma += i
+#     print(i, end=(","))
+#     varSoma += i
    
-print("o valor do somatorio: ", varSoma)
+# print("o valor do somatorio: ", varSoma)
 
 #Questão 04
 
-n = 10
-lista_de_numeros = []
+# n = 10
+# lista_de_numeros = []
 
-for i in range(n):
-    numeros = int(input('Digite os números da lista:'))
-    lista_de_numeros.append(numeros)
+# for i in range(n):
+#     numeros = int(input('Digite os números da lista:'))
+#     lista_de_numeros.append(numeros)
 
-soma = sum(lista_de_numeros)
-print(soma)
+# soma = sum(lista_de_numeros)
+# print(soma)
 
 #Questão 05
 
-valor_somatorio = 0
+# valor_somatorio = 0
 
-for i in range (5):
-    valor =  int(input("Digite um valor: "))
+# for i in range (5):
+#     valor =  int(input("Digite um valor: "))
 
-    if valor < 10:
-        valor_somatorio += valor 
+#     if valor < 10:
+#         valor_somatorio += valor 
 
-print(valor_somatorio)
+# print(valor_somatorio)
+
+#Questão 07 
+
+valor_somatorio_pares = 0
+
+for i in range(5):
+    valor = int(input("Digite um valor: "))
+
+    if valor % 2 == 0:
+        valor_somatorio_pares += valor
+
+print(valor_somatorio_pares)
